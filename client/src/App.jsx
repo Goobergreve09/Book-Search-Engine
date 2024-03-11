b61bb23836eae11033c1d26be1c650bb2de08da2
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-10 22:38:38 -0400

    added apollo provider to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Create an ApolloClient instance
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Replace with your GraphQL server URL
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // Wrap your application with ApolloProvider
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
