ab3695999e35622471b22205a6fd5a69d0d0703f
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-11 05:10:48 -0400

    added mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,5 @@
-require('dotenv').config(); 
-
 const mongoose = require('mongoose');
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
-
-// Connect to MongoDB using the URI
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
