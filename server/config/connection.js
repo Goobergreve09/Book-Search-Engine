847d1eb895f42a68e5111cf5269a4cc088aa0443
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-11 04:54:33 -0400

    testing render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); 
 
-mongoose.connect(process.env.MONGODB_URI);
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+// Connect to MongoDB using the URI
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
