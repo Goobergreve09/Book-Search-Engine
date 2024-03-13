6ffb968c5b2c251c252c2e7f496b16abb76e0d1c
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-13 02:31:32 -0400

    token issue

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -14,16 +14,15 @@ module.exports = {
 
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // If token is provided in headers, extract it
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      console.log(token)
       return res.status(400).json({ message: 'You have no token!' });
     }
 
@@ -31,14 +30,15 @@ module.exports = {
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch (err) {
+      console.error('Invalid token:', err);
+      return res.status(400).json({ message: 'Invalid token!' });
     }
 
     // send to next endpoint
     next();
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
