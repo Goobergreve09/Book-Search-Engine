40e772cfe86fef646b61ffbe77ae981a7b51e885
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-14 05:04:30 -0400

    fixed src location error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const db = require("./config/connection");
-const typeDefs = require("../schemas/typedefs");
+const typeDefs = require("../server/schemas/typedefs");
 const resolvers = require("../schemas/resolvers");
 const { authMiddleware } = require("./utils/auth");
 
