d63276fe9591b0e724afeee913865ef1f344a34f
Goobergreve09 <gregory.greve@yahoo.com>
2024-03-15 02:26:36 -0400

    build

--- server/server.js
+++ server/server.js
@@ -29,12 +29,12 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.use("/", express.static(path.join(__dirname, 'client', 'dist')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
     });
-  }
+}
+
 
   db.once('open', () => {
     app.listen(PORT, () => {
